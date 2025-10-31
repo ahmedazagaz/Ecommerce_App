c504af3b6fee6915e31f7601e6527847c3162245
youssef <elboundati1234@gmail.com>
2025-10-31 23:46:25 +0100

    ajouter table de dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -3,7 +3,7 @@ import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-
+import React from 'react';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -12,6 +12,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard() {
+   
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
@@ -28,7 +29,45 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    
+                     <div className="flex items-center justify-end mb-4">
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="border rounded px-3 py-2 w-64"
+                        />
+                    </div>
+                     {/* Table */}
+            <div className="overflow-x-auto bg-white shadow rounded-lg mx-6">
+                <table className="min-w-full text-sm text-left">
+                    <thead className="border-b bg-gray-50">
+                     <tr className="border-b">
+                         
+                    <th className="px-6 py-3 font-semibold">Orded ID</th>
+                     <th className="px-6 py-3 font-semibold">Status</th>
+                     <th className="px-6 py-3 font-semibold">Customer</th>
+                     <th className="px-6 py-3 font-semibold">Payment Method</th>
+                     <th className="px-6 py-3 font-semibold">Payment Status</th>
+                     <th className="px-6 py-3 font-semibold">Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr  className="border-b hover:bg-gray-100">
+                     <td className="px-6 py-3"></td>
+                    <td className="px-6 py-3"></td>
+                    <td className="px-6 py-3"></td>
+                    <td className="px-6 py-3"></td>
+                    <td className="px-6 py-3"></td>
+                    <td className="px-6 py-3"></td>
+                     </tr>
+                      <tr>
+                     <td colSpan={3} className="text-center py-4 text-gray-500">
+                      No users found
+                    </td>
+                     </tr>  
+                    </tbody>
+                </table>
+            </div>
                 </div>
             </div>
         </AppLayout>
